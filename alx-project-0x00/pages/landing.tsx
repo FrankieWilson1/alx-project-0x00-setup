8b245abc7a033e7799634f8ba6abaaa8a62d2de6
frankiewilson1 <frankwilliamsugwu@gmail.com>
2025-09-09 21:02:33 +0100

    feat: add rendering components

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,9 +1,8 @@
-const Landing: React.FC = () => {
-    return (
-        <div>
-            <h1 className="text-x1 font-extralight">Landing Page</h1>
-        </div>
-    )
+const Landing: React.FC =  () => {
+  return (
+    <div>
+      <h1 className=" text-xl font-extralight">Landing Page</h1>
+    </div>
+  )
 }
-
 export default Landing
