b0282bfee30ee8821caa5b443aa628b85918e437
frankiewilson1 <frankwilliamsugwu@gmail.com>
2025-09-09 20:40:49 +0100

    feat: add basic routing

---
+++ alx-project-0x00/pages/landing.tsx
@@ -0,0 +1,9 @@
+const Landing: React.FC = () => {
+    return (
+        <div>
+            <h1 className="text-x1 font-extralight">Landing Page</h1>
+        </div>
+    )
+}
+
+export default Landing
